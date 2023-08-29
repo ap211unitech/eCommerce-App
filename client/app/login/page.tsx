4af5c5e084a8bdda2bbd210014ea35b0fd417268
Arjun Porwal <65214523+ap211unitech@users.noreply.github.com>
2023-08-29 22:40:39 +0530

    MetaData added for Signin and SignUp pages

--- client/app/login/page.tsx
+++ client/app/login/page.tsx
@@ -1,4 +1,12 @@
+import { Metadata } from "next";
+
 import LoginTemplate from "@/components/templates/Login";
+
+export const metadata: Metadata = {
+  title: "Sign In yourself in Quickmart",
+  description:
+    "Login and get youself the best products from here || eCommerce App for better performance with better tech stack",
+};
 
 const LoginPage = () => {
   return (
--- client/app/register/page.tsx
+++ client/app/register/page.tsx
@@ -1,4 +1,12 @@
+import { Metadata } from "next";
+
 import RegisterTemplate from "@/components/templates/Register";
+
+export const metadata: Metadata = {
+  title: "Register yourself in Quickmart",
+  description:
+    "Please create an account and get youself the best products from here || eCommerce App for better performance with better tech stack",
+};
 
 const RegisterPage = () => {
   return (
