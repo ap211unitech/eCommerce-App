cd42c6f10cb5ec84914e517a4c68fe6eb0c22783
Arjun Porwal <65214523+ap211unitech@users.noreply.github.com>
2023-09-16 13:26:21 +0530

    No more ignoring typescript errors in build mode

--- client/next.config.js
+++ client/next.config.js
@@ -13,13 +13,6 @@ const nextConfig = {
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
-  },
   reactStrictMode: false,
   env: {
     GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
